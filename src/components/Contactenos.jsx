8c2ba24e9c81b1a0b8a00995d97c112bb3d161a6
dabelgarcia2024 <dabel@hotmail.com>
2024-06-11 15:46:55 -0300

    Colocando estilos al formulario

--- src/components/Contactenos.jsx
+++ src/components/Contactenos.jsx
@@ -12,25 +12,24 @@ function Contactenos({ className = "" }) {
           src="/img/oip-1@3x.png"
         />
         <div className={styles.overlayText}>
-        <p>viviendo</p>
-          <p>un dia a la vez</p>
+          <p className={styles.parrafoUno}>viviendo un</p>
+          <p className={styles.parrafoDos}>dia a la vez</p>
         </div>
       </div>
       <div className={styles.formContainer}>
+      <div className={styles.left}>
         <h2 className={styles.title}>Escríbenos, estamos para servirte.</h2>
         <p className={styles.introText}>
           Permítenos servirte, mándanos un mensaje con tus preguntas o dudas,
           con gusto las responderemos a la brevedad posible.
         </p>
+        </div>
+        <div className={styles.right}>
         <div className={styles.field}>
           <label className={styles.label} htmlFor="name">Nombre y Apellido:</label>
           <input className={styles.input} type="text" id="name" placeholder="Nombre y Apellido" />
-        </div>
-        <div className={styles.field}>
           <label className={styles.label} htmlFor="email">Email:</label>
           <input className={styles.input} type="email" id="email" placeholder="Email" />
-        </div>
-        <div className={styles.field}>
           <label className={styles.label} htmlFor="phone">Teléfono:</label>
           <input className={styles.input} type="tel" id="phone" placeholder="Teléfono" />
         </div>
@@ -38,6 +37,7 @@ function Contactenos({ className = "" }) {
           <textarea className={styles.textArea} rows={11} cols={22} placeholder="Tu mensaje aquí..." />
           <button className={styles.submitButton}>Enviar</button>
         </div>
+      </div>
       </div>
     </div>
   );
